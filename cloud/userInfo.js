3ff8a33d0ca10657e03cf0f27428f20385e92071
李进 <TimLi@lijindeMacBook-Pro.local>
2015-08-21 14:33:48 +0800

    firstcommit

---
+++ cloud/userInfo.js
@@ -0,0 +1,51 @@
+/**
+ * 注册：
+ * ApiName : cmSignUp
+ * Go : {phoneNumber:string,password:string}
+ * Back : {status:string}
+ *
+ */
+AV.Cloud.define("cmSignUp",function(request,response){
+    var params = request.params;
+    var username = params.phoneNumber;
+    var password = params.password;
+    var user = new AV.User();
+    user.set("username",username);
+    user.set("password",password);
+    user.signUp(null,{
+        success:function(signUpUser){
+            //返回的数据需要讨论统一
+            response.success(signUpUser);
+        },
+        error:function(error,user){
+            response.error(error);
+        }
+    });
+});
+
+AV.Cloud.define("cmSignIn",function(request,response){
+    var params = request.params;
+    var username = request.params.phoneNumber;
+    var password = params.password;
+
+   // var username = "13127582620";
+    //var password = "135797";
+
+    AV.User.logIn(username, password, {
+        success: function(loginUser) {
+            //返回的数据需要讨论统一
+            response.success(loginUser);
+        },
+        error: function(user,loginErr) {
+            response.error(loginErr);
+        }
+    });
+
+});
+
+
+AV.Cloud.define("test02",function(request,response){
+
+    amaptest();
+
+});
